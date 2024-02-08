4e32b675a38c1a9593452798ac388fce84eb1236
Runador <runador123456@gmail.com>
2024-02-08 02:40:51 +0200

    Adding listsLab/ListManipulationAdvanced.java

--- Fundamentals/listsLab/ListManipulationAdvanced.java
+++ Fundamentals/listsLab/ListManipulationAdvanced.java
@@ -1,4 +1,4 @@
-package listsLab;
+package ListsLab;
 
 import java.util.Arrays;
 import java.util.List;
@@ -9,105 +9,81 @@ public class ListManipulationAdvanced {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Integer> numbersList = Arrays.stream(scanner.nextLine().split(" "))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        List<Integer> numbersList = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
 
         String input = scanner.nextLine();
 
         while (!input.equals("end")) {
 
-            String command = input.split(" ")[0];
+            String[] tokens = input.split(" ");
+            String command = tokens[0];
 
-            switch (command) {
-                case "Contains":
-                    int number = Integer.parseInt(input.split(" ")[1]);
-                    for (int element : numbersList) {
-                        if (numbersList.contains(number)) {
-                            System.out.println("Yes");
-                            break;
-                        } else {
-                            System.out.println("No such number");
-                            break;
+            if (command.equals("Contains")) {
+                int number = Integer.parseInt(tokens[1]);
+                if (numbersList.contains(number)) {
+                    System.out.println("Yes");
+                } else {
+                    System.out.println("No such number");
+                }
+            } else if (command.equals("Print")) {
+                String command2 = tokens[1];
+                if (command2.equals("even")) {
+                    for (int i = 0; i < numbersList.size(); i++) {
+                        if (numbersList.get(i) % 2 == 0) {
+                            System.out.print(numbersList.get(i) + " ");
                         }
                     }
-                    break;
-                case "Print":
-                    String secondCommand = input.split(" ")[1];
-                    switch (secondCommand) {
-                        case "even":
-                            for (int element : numbersList) {
-                                if (element % 2 == 0) {
-                                    System.out.print(element + " ");
-                                }
-                            }
-                            break;
-                        case "odd":
-                            System.out.println();
-                            for (int element : numbersList) {
-                                if (element % 2 == 1) {
-                                    System.out.print(element + " ");
-                                }
-                            }
-                            break;
+                    System.out.println();
+                } else if (command2.equals("odd")) {
+                    for (int i = 0; i < numbersList.size(); i++) {
+                        if (numbersList.get(i) % 2 == 1) {
+                            System.out.print(numbersList.get(i) + " ");
+                        }
                     }
-                    break;
-                case "Get":
-                    String sumInput = input.split(" ")[1];
-                    switch (sumInput) {
-                        case "sum":
-                            int sum = 0;
-                            for (int i = 0; i < numbersList.size(); i++) {
-                                sum += numbersList.get(i);
-                            }
-                            System.out.println();
-                            System.out.println(sum);
+                    System.out.println();
+                }
+            } else if (command.equals("Get") && tokens[1].equals("sum")) {
+                int sum = 0;
+                for (int i = 0; i < numbersList.size(); i++) {
+                    sum += numbersList.get(i);
+                }
+                System.out.println(sum);
+            } else if (command.equals("Filter")) {
+                String condition = tokens[1];
+                int number = Integer.parseInt(tokens[2]);
+                if (condition.equals(">")) {
+                    for (int i = 0; i < numbersList.size(); i++) {
+                        if (number < numbersList.get(i)) {
+                            System.out.print(numbersList.get(i) + " ");
+                        }
                     }
-                    break;
-                case "Filter":
-                    String condition = input.split(" ")[1];
-                    int conditionNumber = Integer.parseInt(input.split(" ")[2]);
-
-                    switch (condition) {
-                        case ">":
-                            for (int element : numbersList) {
-                                if (element > conditionNumber) {
-                                    System.out.print(element + " ");
-                                }
-                            }
-                            System.out.println();
-                            break;
-                        case "<":
-                            for (int element : numbersList) {
-                                if (element < conditionNumber) {
-                                    System.out.print(element + " ");
-                                }
-                            }
-                            System.out.println();
-                            break;
-                        case ">=":
-                            for (int element : numbersList) {
-                                if (element >= conditionNumber) {
-                                    System.out.print(element + " ");
-                                }
-                            }
-                            System.out.println();
-                            break;
-                        case "<=":
-                            for (int element : numbersList) {
-                                if (element <= conditionNumber) {
-                                    System.out.print(element + " ");
-                                }
-                            }
-                            System.out.println();
-                            break;
+                    System.out.println();
+                } else if (condition.equals("<")) {
+                    for (int i = 0; i < numbersList.size(); i++) {
+                        if (number > numbersList.get(i)) {
+                            System.out.print(numbersList.get(i) + " ");
+                        }
                     }
-                    break;
-            }
-                    input = scanner.nextLine();
-
+                    System.out.println();
+                } else if (condition.equals(">=")) {
+                    for (int i = 0; i < numbersList.size(); i++) {
+                        if (number <= numbersList.get(i)) {
+                            System.out.print(numbersList.get(i) + " ");
+                        }
+                    }
+                    System.out.println();
+                } else if (condition.equals("<=")) {
+                    for (int i = 0; i < numbersList.size(); i++) {
+                        if (number >= numbersList.get(i)) {
+                            System.out.print(numbersList.get(i) + " ");
+                        }
+                    }
+                    System.out.println();
+                }
             }
 
+            input = scanner.nextLine();
+        }
     }
-
 }
