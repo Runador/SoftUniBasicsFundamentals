981f3b9e2a0865ec0fd59aaf32d255c6cad57227
Runador <runador123456@gmail.com>
2024-02-15 00:32:45 +0200

    Adding MuOnline

--- Fundamentals/ProgrammingFundamentalsMidExam05/MuOnline.java
+++ Fundamentals/ProgrammingFundamentalsMidExam05/MuOnline.java
@@ -1,58 +1,62 @@
 package ProgrammingFundamentalsMidExam05;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class MuOnline {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         int initialHealth = 100;
         int initialBitcoins = 0;
-        List<String> dungeonRooms = Arrays.stream(scanner.nextLine().split("\\|"))
-                        .collect(Collectors.toList());
-        boolean isNotFinishedGame = false;
 
-        for (int i = 0; i < dungeonRooms.size(); i++) {
-            String[] tokens = dungeonRooms.get(i).split(" ");
-            String command = tokens[0];
-            int number = Integer.parseInt(tokens[1]);
-            if (command.equals("potion")) {
-                int currentHealth = initialHealth;
-                int reminder = 0;
-                if (number < initialHealth) {
-                    if (currentHealth + number > 100) {
-                        initialHealth = 100;
-                        reminder = initialHealth - currentHealth;
-                        System.out.printf("You healed for %d hp.\n", reminder);
-                    } else {
-                        initialHealth += number;
-                        System.out.printf("You healed for %d hp.\n", number);
+        String[] dungeonsRoom = scanner.nextLine().split("\\|");
+
+        for (int room = 0; room < dungeonsRoom.length; room++) {
+            String[] roomString = dungeonsRoom[room].split(" ");
+
+            String command = roomString[0];
+            int number = Integer.parseInt(roomString[1]);
+
+            switch (command) {
+                case "potion":
+                    if (initialHealth > 0 && initialHealth <= 100) {
+                        int reminder = 100 - initialHealth;
+                        if (number + initialHealth > 100) {
+                            initialHealth = 100;
+                            System.out.printf("You healed for %d hp.%n", reminder);
+                        } else {
+                            initialHealth += number;
+                            System.out.printf("You healed for %d hp.%n", number);
+                        }
+
+                        System.out.printf("Current health: %d hp.%n", initialHealth);
                     }
-                }
-                System.out.printf("Current health: %d hp.\n", initialHealth);
-            } else if (command.equals("chest")) {
-                initialBitcoins += number;
-                System.out.printf("You found %d bitcoins.\n", number);
-            } else {
-                String monsterName = command;
-                initialHealth -= number;
-                if (initialHealth > 0) {
-                    System.out.printf("You slayed %s.\n", monsterName);
-                } else {
-                    System.out.printf("You died! Killed by %s.\n", monsterName);
-                    System.out.printf("Best room: %d", i + 1);
-                    isNotFinishedGame = true;
                     break;
-                }
+                case "chest":
+                    initialBitcoins += number;
+                    System.out.printf("You found %d bitcoins.%n", number);
+                    break;
+                default:
+                    if (initialHealth > 0 && initialHealth > number) {
+                        initialHealth -= number;
+                        System.out.printf("You slayed %s.%n", command);
+                    } else {
+                        System.out.printf("You died! Killed by %s.%n", command);
+                        System.out.printf("Best room: %d", room + 1);
+                        return;
+                    }
             }
+
         }
 
-        if (!isNotFinishedGame) {
-            System.out.printf("You've made it!\nBitcoins: %d\nHealth: %d", initialBitcoins, initialHealth);
-        }
+        System.out.println("You've made it!");
+        System.out.printf("Bitcoins: %d%n", initialBitcoins);
+        System.out.printf("Health: %d", initialHealth);
 
+        /*String command = scanner.nextLine();
+        int number = Integer.parseInt(scanner.nextLine());*/
+
+        // rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest
     }
 }
