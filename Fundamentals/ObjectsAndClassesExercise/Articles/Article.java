bf826f18fcaf64cb659a6eebc4b69300be44ed97
Runador <runador123456@gmail.com>
2024-02-23 23:02:09 +0200

    Adding Article.java

--- Fundamentals/ObjectsAndClassesExercise/Articles/Article.java
+++ Fundamentals/ObjectsAndClassesExercise/Articles/Article.java
@@ -1,4 +1,4 @@
-package оbjectsАndClassesExercise.articles;
+package ObjectsAndClassesExercise.Articles;
 
 public class Article {
 
@@ -16,31 +16,28 @@ public class Article {
         return title;
     }
 
-    public void Rename (String newTitle) {
-        this.title = newTitle;
-    }
-
     public String getContent() {
         return content;
-    }
-
-    public void Edit (String newContent) {
-        this.content = newContent;
     }
 
     public String getAuthor() {
         return author;
     }
 
-    public void ChangeAuthor (String newAuthor) {
+    public void Edit(String newContent) {
+        this.content = newContent;
+    }
+
+    public void changeAuthor(String newAuthor) {
         this.author = newAuthor;
     }
 
-    @Override
+    public void rename(String newTitle) {
+        this.title = newTitle;
+    }
+
     public String toString() {
-        // "{title} - {content}: {author}"
         return String.format("%s - %s: %s", getTitle(), getContent(), getAuthor());
-        //return "" + getTitle() + " - " + getContent() + ": " + getAuthor();
     }
 
 }
