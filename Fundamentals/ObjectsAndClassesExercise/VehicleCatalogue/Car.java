f616942c8d319a6804338ad697a5d7216bad36d2
Runador <runador123456@gmail.com>
2024-02-24 01:09:24 +0200

    Deleting VehicleCatalogue

--- Fundamentals/ObjectsAndClassesExercise/VehicleCatalogue/Car.java
+++
@@ -1,9 +0,0 @@
-package ObjectsAndClassesExercise.VehicleCatalogue;
-
-public class Car extends Vehicle{
-
-    public Car(String type, String model, String color, int horsePower) {
-        super(type, model, color, horsePower);
-    }
-
-}
--- Fundamentals/ObjectsAndClassesExercise/VehicleCatalogue/Truck.java
+++
@@ -1,9 +0,0 @@
-package ObjectsAndClassesExercise.VehicleCatalogue;
-
-public class Truck extends Vehicle{
-
-    public Truck(String type, String model, String color, int horsePower) {
-        super(type, model, color, horsePower);
-    }
-
-}
