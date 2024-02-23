fe0b2b08295ca747c8311bbc46e937ad93233104
Runador <runador123456@gmail.com>
2024-02-24 01:10:00 +0200

    Adding VehicleCatalogue

---
+++ Fundamentals/ObjectsAndClassesExercise/VehicleCatalogue/Car.java
@@ -0,0 +1,9 @@
+package ObjectsAndClassesExercise.VehicleCatalogue;
+
+public class Car extends Vehicle{
+
+    public Car(String type, String model, String color, int horsePower) {
+        super(type, model, color, horsePower);
+    }
+
+}
