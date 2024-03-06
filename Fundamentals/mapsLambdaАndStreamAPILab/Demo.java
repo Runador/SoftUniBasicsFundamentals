5e60f9135f208de8e343f2e3786c587eef9ba70c
Runador <runador123456@gmail.com>
2024-03-06 23:14:01 +0200

    changing Demo

--- Fundamentals/mapsLambdaАndStreamAPILab/Demo.java
+++ Fundamentals/mapsLambdaАndStreamAPILab/Demo.java
@@ -1,22 +1,54 @@
-package mapsLambdaАndStreamAPILab;
+package MapsLambdaAndStreamAPILab;
 
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Demo {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        Map<String, Integer> map = new TreeMap<>();
-        map.put("Sergey", 1233456);
-        map.put("Runador", 12314);
-        map.put("Evgeny", 123234);
 
-        map.remove("Evgeny");
+        /*String[] names = scanner.nextLine().split(" ");
 
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.printf("%s -> %d", entry.getKey(), entry.getValue());
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                                .mapToInt(Integer::parseInt).toArray();
+
+        Map<String, Integer> namesMap = new LinkedHashMap<>();
+
+
+        for (String name : names) {
+            for (int number : numbers) {
+                if (!namesMap.containsKey(name)) {
+                    namesMap.put(name, number);
+                }
+            }
         }
+
+        for (Map.Entry<String, Integer> entry : namesMap.entrySet()) {
+            System.out.printf("%s -> %d\n", entry.getKey(), entry.getValue());
+        }*/
+
+        Map<String, List<String>> map = new LinkedHashMap<>();
+
+        List<String> fruits = new ArrayList<>();
+        fruits.add("strawberry");
+        fruits.add("banana");
+        fruits.add("orange");
+        fruits.add("apple");
+
+        map.put("fruits", fruits);
+
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            System.out.printf("%s -> %s", entry.getKey(), String.join(", ", entry.getValue()));
+        }
+
+
+
+
+
+
     }
+
 }
+
