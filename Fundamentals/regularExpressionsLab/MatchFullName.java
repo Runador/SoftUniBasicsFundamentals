18b7f555b09c05c4b4ae6e85f72ca4710248b112
Runador <runador123456@gmail.com>
2024-03-15 00:02:24 +0200

    Adding some

--- Fundamentals/regularExpressionsLab/MatchFullName.java
+++ Fundamentals/regularExpressionsLab/MatchFullName.java
@@ -1,4 +1,4 @@
-package regularExpressionsLab;
+package RegularExpressionsLab;
 
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -8,17 +8,14 @@ public class MatchFullName {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String regexInput = scanner.nextLine();
+        String input = scanner.nextLine();
 
-        Pattern pattern = Pattern.compile("\\b[A-Z][a-z]+ [A-Z][a-z]+\\b");
-        Matcher matcher = pattern.matcher(regexInput);
+        Pattern pattern = Pattern.compile("\\b[A-Z][a-z]+ [A-Z][a-z]+");
+        Matcher matcher = pattern.matcher(input);
 
-        for (int i = 0; i < regexInput.length(); i++) {
-            if (matcher.find()) {
-                System.out.print(matcher.group() + " ");
-            }
+        while (matcher.find()) {
+            System.out.printf("%s ", matcher.group());
         }
 
     }
-
 }
