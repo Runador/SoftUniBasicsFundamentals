8b2625c23778cee8b9c7624f47d30111b7a40544
Runador <runador123456@gmail.com>
2024-03-18 01:52:58 +0200

    Adding ReplaceRepeatingChars.java

---
+++ Fundamentals/textProcessingExercise/ReplaceRepeatingChars.java
@@ -0,0 +1,20 @@
+package TextProcessing.ExerciseTextProcessing;
+
+import java.util.Scanner;
+
+public class ReplaceRepeatingChars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        StringBuilder input = new StringBuilder(scanner.nextLine());
+
+        for (int i = 0; i < input.length() - 1; i++) {
+            if (input.charAt(i) == input.charAt(i + 1)) {
+                input.deleteCharAt(i);
+                i = -1;
+            }
+        }
+
+        System.out.println(input);
+    }
+}
