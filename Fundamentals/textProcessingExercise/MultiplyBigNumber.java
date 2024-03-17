365f1f03fedcdcda19b286b8051e64cf442e9751
Runador <runador123456@gmail.com>
2024-03-18 00:54:36 +0200

    Adding MultiplyBigNumber.java

---
+++ Fundamentals/textProcessingExercise/MultiplyBigNumber.java
@@ -0,0 +1,16 @@
+package TextProcessing.ExerciseTextProcessing;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class MultiplyBigNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        BigInteger num1 = new BigInteger(scanner.nextLine());
+        BigInteger num2 = new BigInteger(scanner.nextLine());
+        BigInteger result = num1.multiply(num2);
+
+        System.out.println(result);
+    }
+}
