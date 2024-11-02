6bc6494e43b44f4ab699abfdfd91263c377e1aba
Runador <runador123456@gmail.com>
2024-11-02 23:56:20 +0200

    Adding file

---
+++ Fundamentals/ReverseStrings.java
@@ -0,0 +1,23 @@
+package P_03_11_24;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ReverseStrings {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String<String> 
+        String output = "";
+        String input = scanner.nextLine();
+
+        String command = input;
+
+        for (int i = command.length() - 1; i >= 0; i--) {
+            output += command.charAt(i);
+
+        }
+
+        for (String s : )
+    }
+}
